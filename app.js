937169b5a956b98828f4cca2b8368ecde2060670
Jim <70465670+jimwhiteford@users.noreply.github.com>
2023-04-22 09:10:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,15 @@ const Hive = require("./models/hiveModel");
 const Breed = require("./models/breedModel");
 const HiveType = require("./models/hiveTypeModel");
 const auth = require("./auth");
+const cors = require("cors");
 
 app.use(express.json({ limit: "52428800" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
