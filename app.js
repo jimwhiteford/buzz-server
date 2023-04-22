f74ea7ad58f421c9192a9ce61e658e99074c770e
Jim <70465670+jimwhiteford@users.noreply.github.com>
2023-04-22 09:25:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const cors = require("cors");
 app.use(express.json({ limit: "52428800" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({origin: ["http://localhost:3000", "https://buzz.onrender.com"],}));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
